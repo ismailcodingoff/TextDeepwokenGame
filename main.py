1e64b8190b89580898ebf0caa047fe03712df20f
Ismail <ismail.mahmoudofficial@gmail.com>
2026-06-09 19:56:46 +0100

    added framework for main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from player import create_player   # if your function is in player.py
+
+def main():
+    player = create_player()
+    print("Player created!")
+    print(player)
+
+if __name__ == "__main__":
+    main()
